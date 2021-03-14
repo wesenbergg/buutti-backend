c88a65d8f0f35e289d8ab3b893ec19fb0e54e7ea
wesenbergg <b.b.j.1998@gmail.com>
2021-03-14 02:00:28 +0200

    hot fix

--- client/src/App.js
+++ client/src/App.js
@@ -81,17 +81,17 @@ const App = () => {
       </div>}
       <form className="p-4 mb-5 card">
         <div className="card-body"><label className="form-label" >Add title:</label>
-        <input className="form-control mb-2" value={title} onChange={({ target }) => setTitle(target.value)}/>
-        <label className="form-label">Add title:</label>
-        <input className="form-control mb-2" value={author} onChange={({ target }) => setAuthor(target.value)}/>
-        <label className="form-label">Add description:</label>
-        <textarea className="form-control mb-2" rows={4}
-          value={description} onChange={({ target }) => setDescription(target.value)}/>
-        
-          <button disabled={!isDisabled()} className="btn btn-success" type="button" onClick={handleSubmit}>Save New</button>
-        <button disabled={isDisabled()} className="btn btn-success" type="button" onClick={handleUpdate}>Save</button>
-        <button disabled={isDisabled()} className="btn btn-outline-danger" type="button" onClick={handleDelete}>Delete</button>
-
+          <input className="form-control mb-2" value={title} onChange={({ target }) => setTitle(target.value)}/>
+          <label className="form-label">Add title:</label>
+          <input className="form-control mb-2" value={author} onChange={({ target }) => setAuthor(target.value)}/>
+          <label className="form-label">Add description:</label>
+          <textarea className="form-control mb-2" rows={4}
+            value={description} onChange={({ target }) => setDescription(target.value)}/>
+          
+          <button disabled={isDisabled()} className="btn btn-success" type="button" onClick={handleSubmit}>Save New</button>
+          <button disabled={!isDisabled()} className="btn btn-success" type="button" onClick={handleUpdate}>Save</button>
+          <button disabled={!isDisabled()} className="btn btn-light" type="button" onClick={reset}>Cancel</button>
+          <button disabled={!isDisabled()} className="btn btn-outline-danger" type="button" onClick={handleDelete}>Delete</button>
         </div>
         
       </form>
